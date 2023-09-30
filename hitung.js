3dbe4649d566206c8fd8fdd3bff22c05b5bc99a2
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmaile.com>
2023-09-30 15:09:29 +0700

    [agus] feat : first commit

---
+++ hitung.js
@@ -0,0 +1,12 @@
+exports.lp = function(sisi){
+    return sisi * sisi;
+};
+exports.kp = function(sisi){
+    return sisi * 4;
+};
+exports.lpp = function(p, l){
+    return p * l;
+};
+exports.kpp = function(p, l){
+    return 2 * (p + l);
+};
